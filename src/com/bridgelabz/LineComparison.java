b0642bcd314d2a8fff925c8f98a8d6b5226d5dcf
Siraj <sirajkhanc2305@gmail.com>
2021-08-18 18:07:01 +0530

    UC2

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -20,6 +20,27 @@ public class LineComparison {
         y2 = y2s.nextInt();
         a = x2 - x1;
         b = y2 - y1;
-        System.out.println("Distance between the given coordinates are : "+Math.sqrt((a * a) + (b * b)));
+        System.out.println("Enter Coordinates of next line : ");
+        System.out.println("Enter x1 of line 2 : ");
+        Scanner x3s = new Scanner(System.in);
+        int x3 = x3s.nextInt();
+        Scanner x4S = new Scanner(System.in);
+        System.out.println("Enter x2 of line 2 : ");
+        int x4 = x4S.nextInt();
+        Scanner y3s = new Scanner(System.in);
+        System.out.println("Enter y1 of line 2 : ");
+        int y3 = y3s.nextInt();
+        System.out.println("Enter y2 of line 2 : ");
+        Scanner y4s = new Scanner(System.in);
+        int y4 = y4s.nextInt();
+        int c = x4 - x3;
+        int d = y4 - y3;
+        double line1 = Math.sqrt((a*a)+(b*b));
+        double line2 = Math.sqrt((c*c)+(d*d));
+        if (line1 == line2)
+        {
+            System.out.println("Both the lines are equal in length.");
+        }
+        else System.out.println("Both lines are unequal in length.");
     }
 }
